46f40befc8dca2066a3c025eda9e0f0c9d915d2c
Diana Arismendi <dianajara82@gmail.com>
2020-08-28 22:28:09 -0500

    mi primer calculador con c#

---
+++ Calculador de Edad/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculador_de_Edad
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int edad;
+            edad = int.Parse(textBox1.Text);
+            if(edad>=18)
+            {
+                label3.Text = "Usted es Mayor de Edad";
+            }
+            else
+            {
+                label3.Text = "Usted es Menor de edad";
+            }
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
